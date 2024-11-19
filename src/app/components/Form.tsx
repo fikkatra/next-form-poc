8d8344bec78dd7d96db83021bce8645fa0947f92
Sophie Traen <sophie_traen@yahoo.com>
2024-11-19 09:47:04 +0100

    corrected typo

--- src/app/components/Form.tsx
+++ src/app/components/Form.tsx
@@ -6,7 +6,7 @@ export function Form() {
   return (
     <form action={submitForm} className='flex flex-col gap-2 items-start'>
       <input name="firstName" id="lastName" placeholder="First name" className='border' />
-      <input name="firstName" id="lastName" placeholder="Last name"  className='border'/>
+      <input name="lastName" id="lastName" placeholder="Last name"  className='border'/>
       <input type="submit" className='border bg-slate-100' />
     </form>
   );
